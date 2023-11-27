6d99b5a96270d39a5a48b2bc74785f170f1265f2
satsuki-64 <15079296601@163.com>
2023-11-28 01:29:29 +0800

    Change CMake File

--- build/CMakeFiles/MainTest_cmake.dir/cmake_clean.cmake
+++ build/CMakeFiles/MainTest_cmake.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
+  "../MainTest_cmake"
+  "../MainTest_cmake.pdb"
   "CMakeFiles/MainTest_cmake.dir/src/EntryPoint.cpp.o"
   "CMakeFiles/MainTest_cmake.dir/src/EntryPoint.cpp.o.d"
   "CMakeFiles/MainTest_cmake.dir/src/compiler/Scan.cpp.o"
   "CMakeFiles/MainTest_cmake.dir/src/compiler/Scan.cpp.o.d"
-  "MainTest_cmake"
-  "MainTest_cmake.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
